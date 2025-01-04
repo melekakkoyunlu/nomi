3658bc3e143f6660e9a911f4909f68c5003c75fe
Melek Akkoyunlu <pikachu.psyduckk@gmail.com>
2025-01-04 23:13:50 +0300

    login forma forget password pop up ı eklendi

--- lib/src/features/authentication/screens/login/widgets/login_form_widget.dart
+++ lib/src/features/authentication/screens/login/widgets/login_form_widget.dart
@@ -10,6 +10,7 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
+      child: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.symmetric(vertical: tFormHeight - 10),
           child: Column(
@@ -31,25 +32,136 @@ class LoginForm extends StatelessWidget {
                   hintText: tPassword,
                   border: OutlineInputBorder(),
                   suffixIcon: IconButton(
-                      onPressed: null,
-                      icon: Icon(Icons.remove_red_eye_sharp)),
+                    onPressed: null,
+                    icon: Icon(Icons.remove_red_eye_sharp),
+                  ),
                 ),
               ),
               const SizedBox(height: tFormHeight - 20),
               Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {
-
-                      },
-                      child: Text(tForgetPassword))),
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      isScrollControlled: true, // Enable scrolling for large content
+                      builder: (context) => SingleChildScrollView(
+                        child: Container(
+                          padding: EdgeInsets.all(tDefaultSize),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tForgetPasswordTitle,
+                                style: Theme.of(context).textTheme.headlineMedium,
+                              ),
+                              Text(
+                                tForgetPasswordSubTitle,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              const SizedBox(height: 30.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  padding: const EdgeInsets.all(20.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Colors.grey.shade200,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.mail_outline_rounded,
+                                        size: 60.0,
+                                      ),
+                                      const SizedBox(width: 10.0),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              tEmail,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headlineSmall,
+                                            ),
+                                            Text(
+                                              tResetViaEMail,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  padding: const EdgeInsets.all(20.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Colors.grey.shade200,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.mobile_friendly_rounded,
+                                        size: 60.0,
+                                      ),
+                                      const SizedBox(width: 10.0),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              tPhoneNo,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headlineSmall,
+                                            ),
+                                            Text(
+                                              tResetViaPhone,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Text(tForgetPassword),
+                ),
+              ),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                    onPressed: () {}, child: Text(tLogin.toUpperCase())),
+                  onPressed: () {},
+                  child: Text(tLogin.toUpperCase()),
+                ),
               )
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
