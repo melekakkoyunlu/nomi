d606da99170be7253d3116fa002aa5501f2a4109
alkncnbks <alkan3797a@gmail.com>
2025-01-14 15:18:07 +0300

    all authentication done

--- lib/src/repository/user_repository/user_repository.dart
+++ lib/src/repository/user_repository/user_repository.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,26 +5,73 @@ import 'package:get/get.dart';
 
 import '../../features/authentication/models/user_model.dart';
 
-class UserRepository extends GetxController{
+class UserRepository extends GetxController {
   static UserRepository get instance => Get.find();
 
-  final _db = FirebaseFirestore.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  createUser(UserModel user) async{
-    await _db.collection("Users").add(user.toJson())
-        .whenComplete(
-        ()=> Get.snackbar("Success", "Your account has been created.",
-            snackPosition: SnackPosition.BOTTOM,
-            backgroundColor: Colors.green.withOpacity(0.1),
-            colorText: Colors.green),
-    )
-    .catchError((error, stackTrace){
-      Get.snackbar("Error","Something went wrong. Try again.",
-            snackPosition: SnackPosition.BOTTOM,
-            backgroundColor: Colors.redAccent.withOpacity(0.1),
-            colorText: Colors.red);
-      print(error.toString());
-    });
+  Future<void> createUser(String email, String password, String fullName, String phoneNo) async {
+    try {
+      // Firebase Authentication ile kullanıcı oluştur
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Kullanıcı başarıyla oluşturulduktan sonra Firestore'a kaydet
+      final user = UserModel(
+        id: userCredential.user!.uid, // Firestore'da kullanıcı kimliği olarak kullanılacak
+        email: email,
+        fullName: fullName,
+        phoneNo: phoneNo,
+        password: password, // Şifreyi sadece örnek olarak ekliyoruz, gerçek uygulamalarda bu şekilde saklanmamalı
+      );
+
+      await _db.collection("Users").doc(user.id).set(user.toJson());
+
+      // Başarılı mesaj
+      Get.snackbar(
+        "Success",
+        "Your account has been created.",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green.withOpacity(0.1),
+        colorText: Colors.green,
+      );
+    } on FirebaseAuthException catch (e) {
+      // Authentication hataları
+      String errorMessage;
+      switch (e.code) {
+        case 'email-already-in-use':
+          errorMessage = "This email is already in use. Please try another one.";
+          break;
+        case 'invalid-email':
+          errorMessage = "The email address is not valid.";
+          break;
+        case 'weak-password':
+          errorMessage = "The password is too weak.";
+          break;
+        default:
+          errorMessage = "An error occurred: ${e.message}";
+      }
+
+      Get.snackbar(
+        "Error",
+        errorMessage,
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.redAccent.withOpacity(0.1),
+        colorText: Colors.red,
+      );
+    } catch (e) {
+      // Genel hatalar
+      Get.snackbar(
+        "Error",
+        "Something went wrong. Please try again.",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.redAccent.withOpacity(0.1),
+        colorText: Colors.red,
+      );
+      print("Error: $e");
+    }
   }
-
 }
